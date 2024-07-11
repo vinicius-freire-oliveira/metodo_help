3ad4ccf6a765b4bd0345ae7afd2c4e0a38292e71
Vinícius-Freire <120794204+vinicius-freire-oliveira@users.noreply.github.com>
2024-07-11 05:53:36 -0300

    Add files via upload
    O método help(), por exemplo, retorna uma descrição sobre uma variável, método ou classe

---
+++ metodo_help.py
@@ -0,0 +1,10 @@
+estudantes_2 = ["João", "Maria", "José", "Ana"] 
+
+# Importando uma função específica de uma biblioteca
+from random import choice
+
+# O método help(), por exemplo, retorna uma descrição sobre uma variável, método ou classe.
+print(help(choice))
+
+estudante = choice(estudantes_2)
+print(estudante)
